74197baa97ec1789dfa6dc5bf82572a603f6c805
Christopher J. Bailey <bailey.cj@gmail.com>
2016-06-23 12:20:59 +0200

    kill logic

--- stormdb/cluster.py
+++ stormdb/cluster.py
@@ -282,7 +282,7 @@ class ClusterBatch(object):
 
     def kill(self, jobid=None):
         for job in self._joblist:
-            if jobid is not None and job._jobid == int(jobid):
+            if jobid is not None and int(job._jobid) == int(jobid):
                 job.kill()
 
     def build_cmd(self):
