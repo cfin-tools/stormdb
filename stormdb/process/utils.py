6d6cdd88bf3d80ed745fb21dd8ae538033915aec
Christopher J. Bailey <bailey.cj@gmail.com>
2017-01-06 15:10:15 +0100

    use mkdtemp instead

--- stormdb/process/utils.py
+++ stormdb/process/utils.py
@@ -15,15 +15,16 @@ import tempfile
 
 def convert_dicom_to_nifti(first_dicom, output_fname,
                            converter='mri_convert'):
-    with tempfile.TemporaryDirectory() as tmpdir:
-        shutil.copytree(os.path.dirname(first_dicom), tmpdir)
-        if converter == 'mri_convert':
-            cmd = ' '.join(converter, first_dicom, output_fname)
-        else:
-            raise NotImplementedError('{:s} not known.'.format(converter))
+    tmpdir = tempfile.mkdtemp()
+    shutil.copytree(os.path.dirname(first_dicom), tmpdir)
+    if converter == 'mri_convert':
+        cmd = ' '.join(converter, first_dicom, output_fname)
+    else:
+        raise NotImplementedError('{:s} not known.'.format(converter))
 
-        try:
-            subp.check_output([cmd], stderr=subp.STDOUT, shell=False)
-        except subp.CalledProcessError as cpe:
-            raise RuntimeError('Conversion failed with error message: '
-                               '{:s}'.format(cpe.returncode, cpe.output))
+    try:
+        subp.check_output([cmd], stderr=subp.STDOUT, shell=False)
+    except subp.CalledProcessError as cpe:
+        raise RuntimeError('Conversion failed with error message: '
+                           '{:s}'.format(cpe.returncode, cpe.output))
+    shutil.rmtree(tmpdir)
