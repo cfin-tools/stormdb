0030d8f1a0bc4082a482e5523d7447f20e521a18
Christopher J. Bailey <bailey.cj@gmail.com>
2017-02-18 23:34:16 +0100

    source, bem and fwd creation clusterised

--- stormdb/process/mne_python.py
+++ stormdb/process/mne_python.py
@@ -1,4 +1,7 @@
-from ..base import check_destination_writable, check_source_readable
+import os
+from .utils import (_get_absolute_proj_path)
+from ..base import (enforce_path_exists, check_destination_writable,
+                    check_source_readable)
 from ..cluster import ClusterBatch
 
 
@@ -30,3 +33,83 @@ class MNEPython(ClusterBatch):
 
         self.add_job(cmd, n_threads=1, job_name='mne.raw.filter')
         self.info['io_mapping'] += [dict(input=in_fname, output=out_fname)]
+
+    def setup_source_space(self, subject, src_fname, **kwargs):
+
+        subjects_dir = self._triage_subjects_dir_from_kwargs(kwargs)
+
+        enforce_path_exists(os.path.join(subjects_dir, subject))
+        if not check_destination_writable(src_fname):
+            raise IOError('Output file {0} not writable!'.format(src_fname))
+
+        script = ("from mne import setup_source_space;"
+                  "setup_source_space('{subject:s}', fname='{src_fname:s}'"
+                  "{kwargs:});")
+        filtargs = ', '.join("{!s}={!r}".format(key, val) for
+                             (key, val) in kwargs.items())
+        filtargs = ', ' + filtargs if len(kwargs) > 0 else filtargs
+        cmd = "python -c \""
+        cmd += script.format(subject=subject, src_fname=src_fname,
+                             kwargs=filtargs)
+        cmd += "\""
+
+        self.add_job(cmd, n_threads=1, job_name='mne.src_space')
+        self.info['io_mapping'] += [dict(input=subject, output=src_fname)]
+
+    def prepare_bem_model(self, subject, bem_fname, **kwargs):
+        subjects_dir = self._triage_subjects_dir_from_kwargs(kwargs)
+        enforce_path_exists(os.path.join(subjects_dir, subject))
+        if not check_destination_writable(bem_fname):
+            raise IOError('Output file {0} not writable!'.format(bem_fname))
+
+        script = ("from mne import (make_bem_model, make_bem_solution, "
+                  "write_bem_solution);\n"
+                  "surfs = make_bem_model('{subject:s}'{kwargs:});\n"
+                  "bem = make_bem_solution(surfs);\n"
+                  "write_bem_solution('{bem_fname:s}', bem)\n")
+        filtargs = ', '.join("{!s}={!r}".format(key, val) for
+                             (key, val) in kwargs.items())
+        filtargs = ', ' + filtargs if len(kwargs) > 0 else filtargs
+        cmd = "python -c \""
+        cmd += script.format(subject=subject, bem_fname=bem_fname,
+                             kwargs=filtargs)
+        cmd += "\""
+
+        self.add_job(cmd, n_threads=1, job_name='mne.prep_bem')
+        self.info['io_mapping'] += [dict(input=subject, output=bem_fname)]
+
+    def make_forward_solution(self, meas_fname, trans_fname, bem_fname,
+                              fwd_fname, **kwargs):
+        for fname in (meas_fname, trans_fname, bem_fname):
+            if not check_source_readable(fname):
+                raise IOError('Input file {} not readable!'.format(fname))
+        if not check_destination_writable(fwd_fname):
+            raise IOError('Output file {} not writable!'.format(bem_fname))
+
+        script = ("from mne import make_forward_solution;\n"
+                  "make_forward_solution('{meas:s}', '{trans:s}', "
+                  "'{bem:s}', fname='{fwd:s}'{kwargs:});\n")
+        filtargs = ', '.join("{!s}={!r}".format(key, val) for
+                             (key, val) in kwargs.items())
+        filtargs = ', ' + filtargs if len(kwargs) > 0 else filtargs
+        cmd = "python -c \""
+        cmd += script.format(meas=meas_fname, trans=trans_fname, bem=bem_fname,
+                             fwd=fwd_fname, kwargs=filtargs)
+        cmd += "\""
+
+        self.add_job(cmd, n_threads=1, job_name='mne.fwd_solve')
+        self.info['io_mapping'] += [dict(input=meas_fname, output=fwd_fname)]
+
+    def _triage_subjects_dir_from_kwargs(self, kwargs):
+        if 'subjects_dir' not in kwargs.keys():
+            if 'SUBJECTS_DIR' in os.environ.keys():
+                subjects_dir = os.environ['SUBJECTS_DIR']
+            else:
+                raise ValueError('No SUBJECTS_DIR defined! You must do so '
+                                 'either by using an argument to this method, '
+                                 'or by setting the SUBJECT_DIR environment '
+                                 'variable. The directory must exist.')
+        else:
+            subjects_dir = _get_absolute_proj_path(subjects_dir,
+                                                   self.proj_name)
+            os.environ['SUBJECTS_DIR'] = subjects_dir
